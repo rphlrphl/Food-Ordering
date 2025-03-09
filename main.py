30d079f446e7bc764ccfc2777bd06f77b6411fd6
JANN RAPHA-EL S. OPEÑA <156049297+rphlrphl@users.noreply.github.com>
2025-03-09 11:28:15 +0800

    Added add food function

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ TO DO LIST:
 """
 
 class MenuList:
-    def __init__(self, item, price):
+    food_menu = {}
+    def __init__(self, id, item, price):
+        self.__id = id
         self.__item = item
         self.__price = price
+        
+    def get_id(self):
+        return self.__id
         
     def get_item(self):
         return self.__item
         
     def get_price(self):
         return self.__price
+        
+    def update_menu(self):
+        self.food_menu.update({self.__id:{'item':self.__item,'price':self.__price}})
         
     def __str__(self):
         return f"{self.__item}: ${self.__price}"
@@ -39,10 +47,16 @@ class Admin(User):
                 price = float(input("Enter the price of the item: "))
                 break
             except Exception: print()
-        menu.append(MenuList(item, price))
-    
-menu = [
-    MenuList('Fries', 9.99),
-    MenuList('Burger',19.99),
-    MenuList('Fried Chicken', 29.99)
-    ]
+        menu = MenuList((len(MenuList.food_menu) + 1), item, price)
+        menu.update_menu()
+
+print(MenuList(00,00,00).food_menu)
+Admin('test').add_item()    
+print(MenuList(00,00,00).food_menu)
+
+
+# menu = [
+#     MenuList('Fries', 9.99),
+#     MenuList('Burger',19.99),
+#     MenuList('Fried Chicken', 29.99)
+#     ]
